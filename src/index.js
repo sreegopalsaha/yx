6b47ed9953f1accdfd5f2207c4a0ef2363147870
sreegopalsaha <sreegopal0101@gmail.com>
2025-03-04 17:16:18 +0530

    removed db

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import dotenv from "dotenv";
-dotenv.config({path: "../.env"});
-import {app} from "./app.js";
-import connectDB from "./configs/db.config.js";
+dotenv.config({ path: "../.env" });
+import { app } from "./app.js";
 
 const PORT = process.env.PORT || 5000;
 
-const startServer = async () => {
-  try {
-    await connectDB();
-    app.listen(PORT, () => {
-      console.log(`⚙️  Server is running on http://localhost:${PORT}`);
-    });
-  } catch (error) {
-    console.error("ERROR WHILE DB CONNECTION:", error.message);
-  }
-};
-
-startServer();
+app.listen(PORT, () => {
+  console.log(`⚙️  Server is running on ${PORT}`);
+});
